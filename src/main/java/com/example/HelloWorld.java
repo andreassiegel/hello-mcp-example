0eb90938dec667b8d541b7b4327b75dd1923a65c
Andreas Siegel <mail@andreassiegel.de>
2025-05-08 11:24:13 +0200

    Füge HelloWorld.java hinzu

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Eine einfache Hello World-Anwendung.
+ */
+public class HelloWorld {
+    
+    /**
+     * Die Hauptmethode, die beim Start des Programms ausgeführt wird.
+     * 
+     * @param args Kommandozeilenargumente (werden nicht verwendet)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello MCP Example World!");
+    }
+}
